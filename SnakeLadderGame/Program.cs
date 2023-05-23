891b98e223c6a084b372525d88ce39a349057e98
Tanaya <desaitanaya17@gmail.com>
2023-05-23 18:46:23 +0530

    [Tanaya] Add : C# Program to implement Snake and Ladder game using C#.

---
+++ SnakeLadderGame/Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace SnakeLadderGame
+{
+    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SnakeLadder snakeLadder = new SnakeLadder();
+
+            Console.WriteLine("Enter Player name :");
+            snakeLadder.playerName = Console.ReadLine();
+
+            snakeLadder.DiceRoll();
+
+        }
+    }
+}
+
